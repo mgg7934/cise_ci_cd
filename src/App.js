e2d335683e2b3d336888ea6ba9ade2b5e933d12c
mgg7934 <57641528+mgg7934@users.noreply.github.com>
2021-04-06 13:19:29 +1200

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React, { Component } from 'react';
- class App extends Component {
+import React, { Component } from "react";
+class App extends Component {
   constructor() {
     super();
     this.state = {
       count: 0,
-    }
- }
- makeIncrementer = amount => () =>
-  this.setState(prevState => ({
-    count: prevState.count + amount,
-  }));
- increment = this.makeIncrementer(1);
+    };
+  }
+  makeIncrementer = (amount) => () =>
+    this.setState((prevState) => ({
+      count: prevState.count + amount,
+    }));
+  increment = this.makeIncrementer(1);
   render() {
     return (
       <div>
-      <p>Count: {this.state.count}</p>
-      <button className="increment" onClick={this.increment}>Increment count</button>
-    </div>
-   )
+        <p>Count: {this.state.count}</p>
+        <button className="increment" onClick={this.increment}>
+          Increment count
+        </button>
+      </div>
+    );
   }
- }
- export default App;
+}
+export default App;
